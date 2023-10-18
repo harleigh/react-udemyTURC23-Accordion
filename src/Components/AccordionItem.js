80b8ff2422254144f57620e410fdefa035cec615
harleigh <harleigh.marsh@gmail.com>
2023-10-18 15:07:57 -0700

    accordion items behave dynamically

--- src/Components/AccordionItem.js
+++ src/Components/AccordionItem.js
@@ -1,10 +1,26 @@
+import { useState } from "react";
+
 export function AccordionItem({ number, title, contents }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const displayNumber = () => {
+    return number < 9 ? "0" + number.toString() : number.toString();
+  };
+
+  const displayIcon = () => {
+    return isOpen === true ? "-" : "+";
+  };
+
+  const handleClick = () => {
+    setIsOpen((prevVal) => !prevVal);
+  };
+
   return (
-    <div className="accordionItem">
-      <p className="accordionNumber">{number}</p>
+    <div className="accordionItem" onClick={handleClick}>
+      <p className="accordionNumber">{displayNumber()}</p>
       <p className="accordionTitle">{title}</p>
-      <p className="accordionIcon">-</p>
-      <div className="accordionContents">{contents}</div>
+      <p className="accordionIcon">{displayIcon()}</p>
+      {isOpen === true && <div className="accordionContents">{contents}</div>}
     </div>
   );
 }
