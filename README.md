cd269a890849721ab045379ae0588cd852ef1c2a
harleigh <harleigh.marsh@gmail.com>
2023-10-18 09:11:31 -0700

    initial commit of project

---
+++ README.md
@@ -0,0 +1,2 @@
+project in the udemy course: The ultimate React Course 2023 by Jonas Schmedtmann.
+* a simple  accordion app
---
+++ src/Components/AccordionApp.js
@@ -0,0 +1,5 @@
+/* Project based on project from TURC23 */
+
+export default function PackingTodoApp() {
+  return <div className="">Accordion App!</div>;
+}
